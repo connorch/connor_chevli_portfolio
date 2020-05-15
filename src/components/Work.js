a7ae23ace386ab45efb03453279393396b885571
Connor Chevli <connorchev@gmail.com>
2020-05-14 23:48:21 -0400

    start building work component shell

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,34 @@
-import React, { makeStyles } from 'react';
-import { Box } from '@material-ui/core';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid, Typography } from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
 
+const experience = {
+  professional: [
+    {
+      companyName: 'Acornpack',
+      title: 'Principal Software Engineer',
+      team: 'B2B Initiative',
+      startDate: new Date("February 2020"),
+    }
+  ]
+
+}
 const Work = () => {
+  const classes = useStyles();
+
   return (
-    <Box></Box>
+    <Grid container className={classes.sectionOne}>
+      <Grid item xs={12}>
+        <Box display="flex" justifyContent="center">
+          <Typography className={classes.test}>Testing123</Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
+const useStyles = makeStyles(theme => ({
+  test: { color: green[400] }
+}));
 
 export default Work;
