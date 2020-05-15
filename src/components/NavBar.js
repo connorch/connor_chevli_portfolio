4c51f3a0594bc92b6065c4a34dc7982312efa63b
Connor Chevli <connorchev@gmail.com>
2020-05-14 23:45:06 -0400

    update button styles on nav click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Button } from '@material-ui/core';
-import { Link as RouterLink } from 'react-router-dom';
-import { green } from '@material-ui/core/colors';
-
-const Pages = {
-  about: 1,
-  work: 2
-}
+import { AppBar, Toolbar, Button, ButtonGroup } from '@material-ui/core';
+import { NavLink as RouterLink } from 'react-router-dom';
+import { blueGrey } from '@material-ui/core/colors';
 
 const NavBar = ({ toggleContactModal }) => {
-  // const [currentPage, setCurrentPage] = useState(Pages.about);
-
   const classes = useStyles();
-  console.log('AOMASDFOIASNDFOASIDNFOIASNFOIASNDFOAISNDF');
 
   return (
     <AppBar elevation={0} color="transparent" position="absolute" className={classes.appBar}>
       <Toolbar className={classes.toolBar} >
-        <Button component={RouterLink} to="/">About</Button>
-        <Button component={RouterLink} to="/work">Work</Button>
-        <Button onClick={toggleContactModal}>Contact</Button>
+        <ButtonGroup variant="text">
+          <Button component={RouterLink} to="/" exact activeClassName={classes.active}>About</Button>
+          <Button component={RouterLink} to="/work" activeClassName={classes.active} >Work</Button>
+          <Button onClick={toggleContactModal}>Contact</Button>
+        </ButtonGroup>
       </Toolbar>
     </AppBar>
   );
@@ -34,7 +28,7 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "center",
   },
   active: {
-    color: green[300]
+    color: theme.palette.secondary.light
   }
 }));
 
