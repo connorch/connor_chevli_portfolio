bd000c93c357fd32988dd195ddc6695f5e3d294d
Connor Chevli <connorchev@gmail.com>
2020-05-14 23:46:39 -0400

    add resume button to about page

--- src/components/AboutSectionTwo.js
+++ src/components/AboutSectionTwo.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Box, Grid, Typography, Button } from '@material-ui/core';
 import portrait from '../assets/portrait_circle.png';
-
+import resumePDF from '../assets/connor_chevli_resume.pdf';
 
 const AboutSectionTwo = () => {
   const classes = useStyles();
@@ -15,6 +15,9 @@ const AboutSectionTwo = () => {
         <Typography variant="h4" color="textSecondary" className={classes.aboutText}>
           I’m a full-stack web developer based in Boston, MA.  I love bringing ideas to life and creating beatiful and intuitive user experiences.
         </Typography>
+      </Box>
+      <Box className={classes.downloadResumeButton}>
+        <Button variant="contained" color="primary" href={resumePDF}>View My Resume</Button>
       </Box>
     </Box>
   );
@@ -51,5 +54,9 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 650,
     textAlign: 'center',
     fontWeight: theme.typography.fontWeightLight
+  },
+  downloadResumeButton: {
+    marginTop: 20,
+    textAlign: 'center',
   }
 }));
