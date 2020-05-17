d0a71cf90032603965c1b5d7e05f4328cd1efb03
Connor Chevli <connorchev@gmail.com>
2020-05-17 13:29:38 -0400

    add footer to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,80 +1,85 @@
-import React from 'react';
-import { Grid, TextField, makeStyles, IconButton, Paper, Box } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { Grid, TextField, makeStyles, IconButton, Paper, Box, Typography } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
+import Footer from './Footer';
 
 const Contact = () => {
   const classes = useStyles();
   return (
-    <Box className={classes.container}>
-      <Paper className={classes.paper}>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <Grid
-            container
-            justify="center"
-            alignItems="center"
-            spacing={3}
-          >
-            <Grid item xs={12}
+    <Fragment>
+      <Box className={classes.container}>
+        <Typography variant="h1" color="textPrimary" align="center" gutterBottom>Say hi!</Typography>
+        <Paper className={classes.paper}>
+          <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <Grid
+              container
+              justify="center"
+              alignItems="center"
+              spacing={3}
             >
-              <TextField
-                variant="outlined"
-                color="secondary"
-                label="Name"
-                id="name"
-                name="name"
-                type="text"
-                className={classes.formField}
-              />
-            </Grid>
-            <Grid item xs={12}
-            >
-              <TextField
-                variant="outlined"
-                color="secondary"
-                label="Email"
-                id="email"
-                name="email"
-                type="email"
-                className={classes.formField}
-                autofocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                color="secondary"
-                label="Message"
-                id="message"
-                name="message"
-                type="text"
-                multiline
-                rows={6}
-                rowsMax={16}
-                className={classes.formField}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <IconButton
-                aria-label="send"
-                className={classes.sendButton}
-                type="submit"
+              <Grid item xs={12}
               >
-                <SendIcon />
-              </IconButton>
+                <TextField
+                  variant="outlined"
+                  color="secondary"
+                  label="Name"
+                  id="name"
+                  name="name"
+                  type="text"
+                  className={classes.formField}
+                />
+              </Grid>
+              <Grid item xs={12}
+              >
+                <TextField
+                  variant="outlined"
+                  color="secondary"
+                  label="Email"
+                  id="email"
+                  name="email"
+                  type="email"
+                  className={classes.formField}
+                  autofocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  color="secondary"
+                  label="Message"
+                  id="message"
+                  name="message"
+                  type="text"
+                  multiline
+                  rows={6}
+                  rowsMax={16}
+                  className={classes.formField}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <IconButton
+                  aria-label="send"
+                  className={classes.sendButton}
+                  type="submit"
+                >
+                  <SendIcon />
+                </IconButton>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Box>
+          </form>
+        </Paper>
+      </Box>
+      <Footer />
+    </Fragment>
   );
 }
 
 const useStyles = makeStyles(theme => ({
   container: {
     backgroundColor: theme.palette.primary.main,
-    minHeight: '100vh',
-    paddingTop: 100
+    paddingTop: 100,
+    paddingBottom: 100
   },
   paper: {
     maxWidth: 500,
